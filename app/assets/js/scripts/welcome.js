c48b7102ff305fbb17e5824fbfd14fba691ba414
SonathPlayer <60995536+SonathPlayer@users.noreply.github.com>
2026-07-11 14:13:28 -0300

    test commit 1107261

--- app/assets/js/scripts/welcome.js
+++
@@ -1,9 +0,0 @@
-/**
- * Script for welcome.ejs
- */
-document.getElementById('welcomeButton').addEventListener('click', e => {
-    loginCancelEnabled(false) // False by default, be explicit.
-    loginViewOnLoginSuccess = VIEWS.landing
-    loginViewOnLoginCancel = VIEWS.login
-    switchView(VIEWS.welcome, VIEWS.login)
-})
